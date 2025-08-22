364ac9e22ed63c5a77315fa43e3dcab0dfabe076
Gabriel Alves <gabrielalves6002@gmail.com>
2025-08-22 14:32:21 -0300

    feat: profile component

---
+++ src/atomic/mol.profile/profile.strings.tsx
@@ -0,0 +1,11 @@
+export const strings = {
+  userDetails: {
+    following: 'Seguindo:',
+    followers: 'Seguidores:',
+    publicRepos: 'Repositório públicos:',
+    bio: 'Bio:',
+    emptyBio: 'Sem história para contar!',
+  },
+  errorMessage: 'Houve algum erro, tente novamente mais tarde.',
+  noExistentUser: 'Usuário não encontrado.',
+};
--- src/data/types/github-user.ts
+++ src/data/types/github-user.ts
@@ -2,4 +2,8 @@ export type GithubUser = {
   login: string;
   avatar_url: string;
   html_url: string;
+  bio?: string;
+  following: number;
+  followers: number;
+  public_repos: number;
 };
