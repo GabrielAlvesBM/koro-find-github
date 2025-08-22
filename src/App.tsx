83607dc03d55c4f6cbe417f5551637df07bcb3bd
Gabriel Alves <gabrielalves6002@gmail.com>
2025-08-22 14:34:32 -0300

    feat: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,52 @@
-import { Button } from '@atomic/atm.button';
-import { SearchInput } from '@atomic/atm.search-input';
+import { useState, type FormEvent } from 'react';
+import { Profile } from '@atomic/mol.profile';
+import { SearchForm } from '@atomic/mol.search-form';
+import type { GithubUser } from '@data/types/github-user';
+import { Status, type StatusType } from '@data/types/status';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [userData, setUserData] = useState<GithubUser | null>(null);
+  const [status, setStatus] = useState<StatusType>(Status.Idle);
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const isSameSearch =
+      userData?.login.toLocaleLowerCase() === username.toLocaleLowerCase();
+    if (!username || isSameSearch) return;
+
+    setStatus(Status.Loading);
+    setUserData(null);
+
+    try {
+      const res = await fetch(`https://api.github.com/users/${username}`);
+      if (!res.ok) {
+        setStatus(Status.NoData);
+        return;
+      }
+      const data: GithubUser = await res.json();
+
+      setUserData(data);
+      setStatus(Status.Success);
+    } catch {
+      setStatus(Status.Error);
+    }
+
+    setUsername('');
+  };
+
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col items-center gap-xs">
-        <SearchInput placeholder={strings.placeholder} />
-        <Button>{strings.search}</Button>
-      </div>
+    <main className="flex flex-col items-center justify-center gap-md h-screen">
+      <Profile data={userData} status={status} />
+
+      <SearchForm
+        onSubmit={handleSubmit}
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
     </main>
   );
 }
-
-const strings = {
-  placeholder: 'Username do Github',
-  search: 'Buscar',
-};
 
 export default App;
