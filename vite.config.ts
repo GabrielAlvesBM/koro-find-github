585fd9288e99f2fcff6e1ec712f8e0d0e30c0e1f
Gabriel Alves <gabrielalves6002@gmail.com>
2025-08-22 13:30:35 -0300

    feat: data path and feedback danger colors

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),
       '@assets': path.resolve(__dirname, 'src/assets'),
+      '@data': path.resolve(__dirname, 'src/data'),
       '@atomic': path.resolve(__dirname, 'src/atomic'),
     },
   },
